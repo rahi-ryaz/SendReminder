4447527492748186ec77206aa272106f925463b3
Rahi Reyaz <rahiryazdar@gmail.com>
2024-07-20 00:59:35 +0530

    Added Notification Ticket Model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express=require("express");
 const bodyParser = require("body-parser");
 const {PORT} =require("./config/serverConfig");
 
-const { sendBasicEmail}= require("./services/email-service");
+const { sendBasicEmail }= require("./services/email-service");
 
 const setupAndStartServer=()=>{
 
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.listen(PORT,()=>{
 
     console.log(`server started at port ${PORT}`);
-    
-    sendBasicEmail(
-        'suppport@admin.com',
-        'eduflixwithrahie@gmail.com',
-        'This is testing email',
-        'Hey how are u , I hope u like the support'
-    )
+
+    // sendBasicEmail(
+    //     'suppport@admin.com',
+    //     'eduflixwithrahie@gmail.com',
+    //     'This is testing email',
+    //     'Hey how are u , I hope u like the support'
+    // )
 
 })
 
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,14 +1,21 @@
 const sender= require("../config/emailConfig");
 
-const sendBasicEmail= (mailFrom , mailTo,mailSubject,mailBody)=>{
-    sender.sendMail({
+const sendBasicEmail= async(mailFrom , mailTo,mailSubject,mailBody)=>{
+    try{
+        const response=await sender.sendMail({
         from:mailFrom,
         to:mailTo,
         subject:mailSubject,
         text:mailBody,
+   });
+    console.log(response);
 
-    });
+} catch(error) {
+    console.log(error);
+
 }
+}
+
 
 module.exports={
     sendBasicEmail
