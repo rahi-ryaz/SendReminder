8f906b5d11eeea6f50cd23917d631db71a45bfe4
Rahi Reyaz <rahiryazdar@gmail.com>
2024-07-24 00:51:23 +0530

    implemented mesaage queues

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
-const express=require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const bodyParser = require("body-parser");
-const {PORT} =require("./config/serverConfig");
+const { PORT } = require('./config/serverConfig');
 
 const TicketController = require('./controllers/ticket-controller');
-//const { sendBasicEmail }= require("./services/email-service");
-const jobs =require("./utils/job");
+const EmailService = require('./services/email-service');
 
-const setupAndStartServer=()=>{
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
-const app= express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/api/v1/tickets', TicketController.create);
+    app.post('/api/v1/tickets', TicketController.create);
 
-app.listen(PORT,()=>{
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
 
-    console.log(`server started at port ${PORT}`);
-    jobs();
-
-    // sendBasicEmail(
-    //     'suppport@admin.com',
-    //     'eduflixwithrahie@gmail.com',
-    //     'This is testing email',
-    //     'Hey how are u , I hope u like the support'
-    // )
-
-})
-
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+        // jobs();
+        
+    });
 }
-
 
 setupAndStartServer();
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -46,6 +46,22 @@ const createNotification = async (data) => {
     }
 }
 
+const subscribeEvents = async (payload) => {
+    let service = payload.service;
+    let data = payload.data;
+    switch(service) {
+        case 'CREATE_TICKET':
+            await createNotification(data);
+            break;
+        case 'SEND_BASIC_MAIL':
+            await sendBasicEmail(data);
+            break;
+        default: 
+            console.log('No valid event received');
+            break;
+    }
+}
+
 
 
 module.exports = {
@@ -53,6 +69,7 @@ module.exports = {
     fetchPendingEmails,
     createNotification,
     updateTicket,
+    subscribeEvents
 }
 
 
